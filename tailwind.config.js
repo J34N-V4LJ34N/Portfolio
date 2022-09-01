c620ce7150caa2ea8c46a3631822d3f089f72f33
J34N-V4LJ34N <jeanmani2003@gmail.com>
2022-09-01 19:20:39 +0530

    Resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,18 @@ module.exports = {
       colors: {
         body: "#0B132B",
         "selected-text": "#EFF1C5",
+        "selected-text-2": "#C1FC95",
+        "selected-text-3": "#D9BBF9",
+        "selected-text-4": "#F3D2DE",
+        "selected-text-5": "#FFD2B2",
         theme: "#3F3FFF",
         nav: "#1C254177",
+        "nav-heading": "#1C2541d6",
         secondary: "#9191A4",
         button: "#84CAE7",
         "button-text": "#041B15",
         input: "#2A2A35",
+        card: "#136F63",
       },
       width: {
         desk: "500px",
